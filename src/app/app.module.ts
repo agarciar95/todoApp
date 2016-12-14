babeaadc0faba39fafc2a99d21275df465db7139
desarrollo.residentes@prograf.mx <desarrollo.residentes@prograf.mx>
2016-12-13 18:07:50 -0600

    se agregaron paginas de checklist y listado de items, tambien se comenzo con el desarrollo de la pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
 import { HomePage } from '../pages/home/home';
+import { IntroPage } from '../pages/intro/intro';
+import { ChecklistPage } from '../pages/checklist/checklist';
+
+import { Datos } from '../providers/datos';
+import { Storage } from '@ionic/storage';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    IntroPage,
+    ChecklistPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +23,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    IntroPage,
+    ChecklistPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},Storage,Datos]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
+
+import { ChecklistPage } from '../checklist/checklist';
+import { ChecklistModel } from '../../models/checklist-model';
+import { Datos } from '../../providers/datos';
+import { Keyboard } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +13,58 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  checklists: ChecklistModel[] = [];
+
+  constructor(public navCtrl: NavController, public dataService: Datos, public alertCtrl: AlertController, public platform: Platform) {
+    this.navCtrl = navCtrl;
+    this.dataService = dataService;
+  }
+
+  ionViewDidLoad(){}
+
+  addChecklist():void{
+    let prompt = this.alertCtrl.create({
+      title: 'Nueva Lista',
+      message: 'Ingresa el nombre de tu lista aqui abajo',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar'
+        },
+        {
+          text: 'Guardar',
+          handler: data =>{
+            let newChecklist = new ChecklistModel(data.name,[]);
+            this.checklists.push(newChecklist);
+
+            newChecklist.checklist.subscribe(update => {this.save()});
+            this.save();
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  renameChecklist(checklist):void{
+
+  }
+
+  viewChecklist(checklist):void{
+
+  }
+
+  removeChecklist(checklist):void{
+
+  }
+
+  save():void{
+
   }
 
 }
