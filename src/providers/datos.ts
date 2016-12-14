4abd051688dbe58acdd9b34e0b3f201e7fca9127
desarrollo.residentes@prograf.mx <desarrollo.residentes@prograf.mx>
2016-12-14 17:17:04 -0600

    proveedor de informacion via local storage

--- src/providers/datos.ts
+++ src/providers/datos.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -11,8 +12,26 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Datos {
 
-  constructor(public http: Http) {
+  constructor(public storage: Storage) {
     console.log('Hello Datos Provider');
+  }
+
+  getData(): Promise<any>{
+    return this.storage.get('checklists');
+  }
+
+  save(data):void{
+    let saveData = [];
+
+    data.forEach( (checklist) => {
+      saveData.push({
+        title: checklist.title,
+        items: checklist.items
+      });
+    });
+
+    let newData = JSON.stringify(saveData);
+    this.storage.set('checklists',newData);
   }
 
 }
