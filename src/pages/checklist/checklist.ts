334904eb87e1dae3e4d6380912f49c030d16b2e1
desarrollo.residentes@prograf.mx <desarrollo.residentes@prograf.mx>
2016-12-14 17:15:39 -0600

    componente de pantalla  de tareas a realizar de una lista - ya funcionando

--- src/pages/checklist/checklist.ts
+++ src/pages/checklist/checklist.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, NavParams, AlertController, ToastController} from 'ionic-angular';
 
 /*
   Generated class for the Checklist page.
@@ -13,10 +13,91 @@ import { NavController } from 'ionic-angular';
 })
 export class ChecklistPage {
 
-  constructor(public navCtrl: NavController) {}
+  checklist: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public toast: ToastController) {
+    this.checklist = this.navParams.get('checklist');
+  }
 
   ionViewDidLoad() {
     console.log('Hello ChecklistPage Page');
+  }
+
+  presentToast(message) {
+    let toast = this.toast.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  addItem():void{
+    let prompt = this.alertCtrl.create({
+        title:'Agregar Tarea',
+        message: 'Anota la tarea a realizar',
+        inputs: [
+          {name:'name'}
+        ],
+        buttons: [
+          {text: 'Cancelar'},
+          {
+            text: 'Agregar',
+            handler: data =>{
+              this.checklist.addItem(data.name);
+              this.presentToast("Tarea agregada!");
+            }
+          }
+        ]
+    });
+
+    prompt.present();
+  }
+
+  toggleItem(item):void{
+    this.checklist.toggleItem(item);
+    (item.checked) ? this.presentToast("Tarea completada!") : '';
+  }
+
+  removeItem(item):void{
+    this.checklist.removeItem(item);
+    this.presentToast("Tarea removida!");
+  }
+
+  renameItem(item):void{
+    let prompt = this.alertCtrl.create({
+      title: 'Renombrar tarea',
+      message: 'Introduce nuevo nombre para la tarea',
+      inputs: [{name: 'name'}],
+      buttons: [
+        { text: 'Cancelar' },
+        {
+          text: 'Actualizar',
+          handler: data => {
+            this.checklist.renameItem(item,data.name);
+            this.presentToast("Tarea renombrada!");
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  uncheckItems():void{
+    // for(let item of this.checklist.items){
+    //   if(item.checked){
+    //     console.log("item-checked:");
+    //   }else{
+    //     console.log("item-no-checked:");
+    //   }
+    //
+    // }
+    this.checklist.items.forEach((item)=>{
+      if(item.checked){
+        this.checklist.toggleItem(item);
+        this.presentToast("Tareas Desmarcadas!");
+      }
+    })
   }
 
 }
