01c391434dc85833aa45483abc12058cfc3ec3ee
desarrollo.residentes@prograf.mx <desarrollo.residentes@prograf.mx>
2016-12-14 17:15:04 -0600

    componente de pantalla principal de listas de tareas - ya funcionando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
 import { NavController, AlertController, Platform } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { ToastController } from 'ionic-angular';
 
 import { ChecklistPage } from '../checklist/checklist';
 import { ChecklistModel } from '../../models/checklist-model';
 import { Datos } from '../../providers/datos';
 import { Keyboard } from 'ionic-native';
+import { IntroPage } from '../intro/intro';
 
 @Component({
   selector: 'page-home',
@@ -15,12 +18,49 @@ export class HomePage {
 
   checklists: ChecklistModel[] = [];
 
-  constructor(public navCtrl: NavController, public dataService: Datos, public alertCtrl: AlertController, public platform: Platform) {
+  constructor(public navCtrl: NavController, public dataService: Datos, public alertCtrl: AlertController,public storage: Storage, public platform: Platform, public toast: ToastController) {
     this.navCtrl = navCtrl;
     this.dataService = dataService;
   }
 
-  ionViewDidLoad(){}
+  ionViewDidLoad(){
+    this.platform.ready().then(()=>{
+      //para redireccionar por primera vez al tutorial
+      this.storage.get('introShown').then((result) => {
+        if(!result){
+          this.storage.set('introShown',true);
+          this.navCtrl.setRoot(IntroPage);
+        }
+      });
+
+      this.dataService.getData().then((checklists) =>{
+        let savedChecklists: any = false;
+
+        if(typeof (checklists) != "undefined"){
+          savedChecklists = JSON.parse(checklists);
+        }
+
+        if(savedChecklists){
+          savedChecklists.forEach((savedChecklist) =>{
+            let loadChecklist = new ChecklistModel(savedChecklist.title,savedChecklist.items);
+            this.checklists.push(loadChecklist);
+
+            loadChecklist.checklist.subscribe(update => {
+              this.save();
+            });
+          })
+        }
+      });
+    });
+  }
+
+  presentToast(message) {
+    let toast = this.toast.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
 
   addChecklist():void{
     let prompt = this.alertCtrl.create({
@@ -43,6 +83,7 @@ export class HomePage {
 
             newChecklist.checklist.subscribe(update => {this.save()});
             this.save();
+            this.presentToast("Lista agregada!");
           }
         }
       ]
@@ -52,18 +93,49 @@ export class HomePage {
   }
 
   renameChecklist(checklist):void{
+    let prompt = this.alertCtrl.create({
+      title: 'Editar lista',
+      message: 'Renombra tu lista',
+      inputs: [
+        { name: 'name' }
+      ],
+      buttons: [
+        {text: 'Cancelar'},
+        {
+          text: 'Actualizar',
+          handler: data =>{
+            let index = this.checklists.indexOf(checklist);
 
+            if(index > -1){
+              this.checklists[index].setTitle(data.name);
+              this.save();
+              this.presentToast("Lista Renombrada!");
+            }
+          }
+        }
+      ]
+    });
+
+    prompt.present();
   }
 
   viewChecklist(checklist):void{
-
+    this.navCtrl.push(ChecklistPage,{checklist: checklist});
   }
 
   removeChecklist(checklist):void{
+    let index = this.checklists.indexOf(checklist);
 
+    if(index > -1){
+      this.checklists.splice(index,1);
+      this.save();
+      this.presentToast("Lista removida!");
+    }
   }
 
   save():void{
+    Keyboard.close();
+    this.dataService.save(this.checklists);
 
   }
 
